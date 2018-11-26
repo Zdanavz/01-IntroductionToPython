e28a25b4b46fcdd89c64b43b7a0d6e88876b9725
zdanavz <zdanaviz@rose-hulman.edu>
2018-11-26 12:57:51 -0500

    Done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zack Zdanavicius.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
-
 ########################################################################
-# TODO: 2.
+#
+# Done: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,33 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+
+window=rg.TurtleWindow()
+blue=rg.SimpleTurtle('turtle')
+blue.pen=rg.Pen('Blue', 5)
+blue.speed=25
+
+size = 150
+
+for k in range(8):
+    blue.draw_circle(size)
+    blue.pen_up()
+    blue.right(50)
+    blue.pen_down()
+    size=size-5
+
+#########################################################################
+
+window.tracer(100)
+
+red=rg.SimpleTurtle('turtle')
+red.pen=rg.Pen('red', 1)
+red.speed=25
+
+for k in range(500):
+    red.left(91)
+    red.forward(k)
+
+
+window.close_on_mouse_click()
